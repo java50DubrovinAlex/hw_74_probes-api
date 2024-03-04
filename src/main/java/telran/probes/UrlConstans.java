39fe7027a8fe6fc95fbacca5b31abbe854a72ce0
Alexander <caxagsh@gmail.com>
2024-03-04 18:31:08 +0200

    hw_75

--- src/main/java/telran/probes/UrlConstans.java
+++ src/main/java/telran/probes/UrlConstans.java
@@ -1,6 +1,6 @@
 package telran.probes;
 
 public interface UrlConstans {
-	String SENSOR_RANGE = "range/sensor";
-	String SENSOR_EMAILS = "emails/sensor";
+	String SENSOR_RANGE = "range/sensor/";
+	String SENSOR_EMAILS = "emails/sensor/";
 }
